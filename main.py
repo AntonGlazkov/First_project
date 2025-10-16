cf8c18d51deb46420fe79fd743fa4e498db8f59e
Anton Hlazkov <a.glazkoff@gmail.com>
2025-10-16 21:08:54 +0300

    Second

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ def get_days_from_today(date):
 
 print(get_days_from_today("2021-10-10"))
 
+import random
+
+def get_numbers_ticket(min_value, max_value, quantity):
+    if (
+        (max_value - min_value + 1) < quantity
+        or quantity <=0
+        or min_value >= max_value
+        or min_value < 1
+        or max_value > 1000
+        ):
+        return []
+    numbers = random.sample(range(min_value, max_value + 1), quantity)
+    return sorted(numbers)
+
+lottery_numbers = get_numbers_ticket(1, 1000, 8)
+print("Ваші лотерейні числа:", lottery_numbers)
