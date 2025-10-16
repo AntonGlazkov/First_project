0c9d19db5dc3ceafe40df8336e627a7951674ae4
Anton Hlazkov <a.glazkoff@gmail.com>
2025-10-16 18:36:07 +0300

    dz_4.1

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("Hello World")
-print("Hello Git")
+from datetime import datetime
+
+def get_days_from_today(date):
+    datetime_object = datetime.strptime(date, "%Y-%m-%d").date()
+    today = datetime.today().date()
+    delta = today - datetime_object
+    return delta.days
+
+print(get_days_from_today("2021-10-10"))
